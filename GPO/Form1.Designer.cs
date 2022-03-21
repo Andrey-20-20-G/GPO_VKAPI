72f0cdc41a4c7ff86a860a63e75007a6f27b2172
megaf <mega.fo@list.ru>
2022-03-21 11:39:58 +0300

    Добавьте файлы проекта.

---
+++ GPO/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace GPO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Auth = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.gettingMembers = new System.Windows.Forms.Button();
+            this.idGroupBox = new System.Windows.Forms.TextBox();
+            this.ShowMembers = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Auth
+            // 
+            this.Auth.Location = new System.Drawing.Point(3, 289);
+            this.Auth.Name = "Auth";
+            this.Auth.Size = new System.Drawing.Size(92, 23);
+            this.Auth.TabIndex = 0;
+            this.Auth.Text = "авторизация";
+            this.Auth.UseVisualStyleBackColor = true;
+            this.Auth.Click += new System.EventHandler(this.Auth_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(297, 22);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(245, 251);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // gettingMembers
+            // 
+            this.gettingMembers.Location = new System.Drawing.Point(101, 289);
+            this.gettingMembers.Name = "gettingMembers";
+            this.gettingMembers.Size = new System.Drawing.Size(181, 23);
+            this.gettingMembers.TabIndex = 2;
+            this.gettingMembers.Text = "Показать информацию о группе";
+            this.gettingMembers.UseVisualStyleBackColor = true;
+            this.gettingMembers.Click += new System.EventHandler(this.gettingMembers_Click);
+            // 
+            // idGroupBox
+            // 
+            this.idGroupBox.Location = new System.Drawing.Point(13, 22);
+            this.idGroupBox.Name = "idGroupBox";
+            this.idGroupBox.Size = new System.Drawing.Size(100, 20);
+            this.idGroupBox.TabIndex = 3;
+            // 
+            // ShowMembers
+            // 
+            this.ShowMembers.Location = new System.Drawing.Point(288, 289);
+            this.ShowMembers.Name = "ShowMembers";
+            this.ShowMembers.Size = new System.Drawing.Size(124, 23);
+            this.ShowMembers.TabIndex = 4;
+            this.ShowMembers.Text = "Показать участников";
+            this.ShowMembers.UseVisualStyleBackColor = true;
+            this.ShowMembers.Click += new System.EventHandler(this.ShowMembers_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(554, 319);
+            this.Controls.Add(this.ShowMembers);
+            this.Controls.Add(this.idGroupBox);
+            this.Controls.Add(this.gettingMembers);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Auth);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Auth;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button gettingMembers;
+        private System.Windows.Forms.TextBox idGroupBox;
+        private System.Windows.Forms.Button ShowMembers;
+    }
+}
+
---
+++ GPO/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VkNet;
+using System.IO;
+using VkNet.Enums.Filters;
+using VkNet.Model;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using xNet;
+using VkNet.Model.RequestParams;
+using VkNet.Enums.SafetyEnums;
+
+namespace GPO
+{
+    public partial class Form1 : Form
+    {
+        VkApi api = new VkApi();
+        long User_Indentity = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        /*авторизация для того, чтобы получить опции АПИ ВК */
+        private void Auth_Click(object sender, EventArgs e)
+        {
+            ApiAuthParams parameters = new ApiAuthParams()
+            {
+                AccessToken = "0de720100de720100de72010fd0d9ca82500de70de720106fc1dfd4e135711be7310dda"
+            };
+            api.Authorize(parameters);
+        }
+        /*Получение информации о группе: id, count of members, name*/
+        public void getGroupInfo()
+        {
+            var group = api.Groups.GetById(null, idGroupBox.Text, null).FirstOrDefault();
+            if (group == null)
+            {
+                return;
+            }
+            int cntMem = getCountOfGroup();
+            listBox1.Items.Add("Идентификатор группы: "+group.Id);
+            listBox1.Items.Add("Название: " + group.Name);
+            listBox1.Items.Add("Количество участников: " + cntMem);
+            listBox1.Items.Add("--------------------------------------------------------------------------------");
+        }
+        /*Вывод в лист бокс участников группы*/
+        public void getMembers()
+        {
+            var groupMembers = api.Groups.GetMembers(new GroupsGetMembersParams()
+            {
+                GroupId = idGroupBox.Text,
+                Sort = GroupsSort.IdAsc,
+            });
+            var user_ids = new List<long>();//список id пользователей
+            foreach (var user in groupMembers)
+            {
+                user_ids.Add(user.Id);//добавляем id список с id пользователей
+                //var _user = api.Users.Get(new long[] { user.Id }).FirstOrDefault();
+                //if (_user == null)
+                //    return;
+                //listBox1.Items.Add(_user.FirstName + " " + _user.LastName + " " + _user.Id);//вывод id в listbox
+            }
+            var maxtrixOfCrossedFriends = new int [user_ids.Count,user_ids.Count];//матрица для графа
+            for (int i = 0; i < user_ids.Count; i++)
+            {
+                var polsovatel = api.Users.Get(new long[] {user_ids[i]}).FirstOrDefault();
+                if (polsovatel.IsClosed == true)
+                {
+                    for (int k = 0; k < user_ids.Count; k++)
+                    {
+                        maxtrixOfCrossedFriends[i, k] = 0;
+                    }
+                    break;
+                }
+                if (polsovatel.IsDeactivated == true)
+                {
+                    for (int k = 0; k < user_ids.Count; k++)
+                    {
+                        maxtrixOfCrossedFriends[i, k] = 0;
+                    }
+                    break;
+                }
+                var users_Friends = api.Friends.Get(new VkNet.Model.RequestParams.FriendsGetParams
+                {
+                    UserId = user_ids[i],
+                });
+                for (int j = 0; j < users_Friends.Count; j++)
+                {
+                    if (user_ids.Contains(users_Friends[j].Id)==true)
+                    {
+                        maxtrixOfCrossedFriends[i, j] = 1;
+                    }
+                    else
+                    {
+                        maxtrixOfCrossedFriends[i, j] = 0;
+                    }
+                }
+            }
+            string mt = "";
+            for (int i=0; i< user_ids.Count; i++)
+            {
+                for (int j = 0; j < user_ids.Count; j++)
+                {
+                    if (maxtrixOfCrossedFriends[i, j]==1)
+                    {
+                        mt += maxtrixOfCrossedFriends[i, j].ToString();
+                    }
+                }
+            }
+            MessageBox.Show(mt);
+        }
+        /*Получение количества участников группы, !!!на прямую не получается взять его!!!*/
+        public int getCountOfGroup()
+        {
+            int count = 0;
+            try
+            {
+                var groupMembers = api.Groups.GetMembers(new GroupsGetMembersParams()
+                {
+                    GroupId = idGroupBox.Text,
+                });
+                count = groupMembers.Count;
+                return count;
+            }
+            catch
+            {
+                MessageBox.Show("Unable to get count of members");
+                return count;
+            }
+            
+        }
+        private void gettingMembers_Click(object sender, EventArgs e)
+        {
+            getGroupInfo();
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            User_Indentity = long.Parse(listBox1.Text.Substring(listBox1.Text.LastIndexOf(" ")));
+            MessageBox.Show(User_Indentity.ToString());
+        }
+        private void ShowMembers_Click(object sender, EventArgs e)
+        {
+            getMembers();
+        }
+    }
+}
